87833759fae86ad41b976d37a3146d120bbfd2c7
dengyi <dengyi@dengyi.pro>
2020-07-28 23:33:21 +0800

    add login

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,11 @@
 <template>
   <div id="app">
-     <a-button type="primary">
-      Primary
-    </a-button>
     <router-view/>
   </div>
 </template>
 
 <style lang="scss">
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+ height: 100%;
 }
 </style>
---
+++ src/views/Login.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <div class="login-box">
+      hhe
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Login',
+  data () {
+    return {}
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+  .login-box {
+    width: 700px;
+    height: 400px;
+    background-color: black;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+  }
+
+</style>
